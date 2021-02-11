57dc22095d8d941e090789569cf6da3451826bae
James Rogers <jarogers095@gmail.com>
2021-02-10 22:34:50 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={(event) => {event.target.style.backgroundColor = 'red'}} />
   </div>,
   document.getElementById('root')
 );
